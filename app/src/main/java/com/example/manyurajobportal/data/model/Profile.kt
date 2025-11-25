14d5093399b64ff4e6d52092f7711bee4832a652
Gideon Mwangi <gideonmwangi718@gmail.com>
2025-11-25 14:52:47 +0300

    Added a new model of the profile

---
+++ app/src/main/java/com/example/manyurajobportal/data/model/Profile.kt
@@ -0,0 +1,8 @@
+package com.example.manyurajobportal.data.model
+
+data class Profile(
+    val fullName: String = "",
+    val email: String = "",
+    val phone: String = "",
+    val location: String = ""
+)
